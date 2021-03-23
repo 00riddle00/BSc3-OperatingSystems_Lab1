680e811f6435c9ec12bdbd477cfd69429eb49dae
linas <linas.bisikirskas@gmail.com>
2021-03-23 18:44:19 +0200

    Be main

--- main.py
+++ main.py
@@ -1,40 +1,8 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
 
 import sys
 
 from realmachine import RealMachine
 
-# import wx
-
-def main(argv=None):
-    """
-    """
-
-    if not argv:
-        argv = sys.argv[:]
-
-    gui = False
-    if len(argv) == 2:
-        file = argv[1]
-    elif len(argv) == 3 and argv[1] == 'gui':
-        gui = True
-        file = argv[2]
-    else:
-        print('Naudojimas: pyemu <failas>')
-        sys.exit(1)
-
-    rm = RealMachine()
-
-    if gui:
-        # app = wx.App()
-        # frame = wx.Frame(None, -1, 'Bla')
-        # frame.Show()
-        # app.MainLoop()
-        pass
-    else:
-        rm.load_virtual_machine(file)
-        rm.processor.execute()
 
 file = 'test_program_1'
 rm = RealMachine()
